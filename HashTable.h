e874e012c6c9aeaa784a1f63e3ce13527a2d49a2
pyrosenic33 <149697320+pyrosenic33@users.noreply.github.com>
2024-12-04 20:52:11 -0500

    updated insertAll to take in a string filename parameter

--- HashTable.h
+++ HashTable.h
@@ -9,6 +9,7 @@ using namespace std;
 class HashTable{
     int BUCKET_SIZE;
     float MAX_LOAD_FACTOR;
+    //load factor = size of map / size of vector (bucket size)
 
     vector<string> vec;
     unordered_map<int, string> table;
@@ -25,6 +26,6 @@ public:
     void resizeVector();
     void rehashWords();
     void insertWord(string word);
-    void insertAll();
+    void insertAll(string filename);
     bool searchWord(string input);
 };
