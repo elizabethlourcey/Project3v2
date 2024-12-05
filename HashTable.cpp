c2e494d087b33bc594824e36925c7f321c1ddad9
pyrosenic33 <149697320+pyrosenic33@users.noreply.github.com>
2024-12-04 20:51:03 -0500

    updated file with comments

--- HashTable.cpp
+++ HashTable.cpp
@@ -9,6 +9,7 @@ HashTable::HashTable(){
     }
 }
 
+//VVVVVVVVVVVV Accessor functions for testing the hash table w/o testing the whole project VVVVVVVVVV
 int HashTable::getBucketSize(){
     return BUCKET_SIZE;
 }
@@ -25,6 +26,9 @@ vector<string> HashTable::getVec(){
     return vec;
 }
 
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+//basic exponential function to calculate base^exp
 int power(int base, int exp){
     if(exp == 0){
         return 1;
@@ -33,6 +37,11 @@ int power(int base, int exp){
     return base * power(base, exp - 1);
 }
 
+//returns an int key corresponding to the given word
+//key = sum of powers of 26 = letter * base^i (base = 26)
+//  i = 0, 1, 2, etc. (increments up by 1 for each successive letter in the string)
+//  letter = alphabetic rank of each letter: a->1, b->2, c->3, etc.
+//computeKey("abc") = 1*26^0 + 2*26^1 + 3*26^2
 int HashTable::computeKey(string s){
     int key = 0;
     const int BASE = 26;
@@ -50,10 +59,14 @@ int HashTable::computeKey(string s){
     return key;
 }
 
+//simple hash function returning key mod bucket_size: O(1)
 int HashTable::hashFunc(int k){
     return k % BUCKET_SIZE;
 }
 
+//uses quadratic probing to resolve collisions
+//if the given index is already filled, the probe looks at (index + 1) % bucketsize,
+//  (index + 4) % bucketsize, (index + 9) % bucketsize, etc. until empty bucket is found
 int HashTable::collisionReduction(int index){
     //quadratic probing
     //index = hashFunc(k)
@@ -73,8 +86,42 @@ int HashTable::collisionReduction(int index){
     return newIndex;
 }
 
-void HashTable::insertWord(string word){
+//doubles the size of the vector of words and doubles the BUCKET_SIZE attribute to match the new
+//  vector size
+//called when the current load factor exceeds the maximum
+void HashTable::resizeVector(){
+    for(int i = BUCKET_SIZE; i < BUCKET_SIZE * 2; i++){
+        vec.push_back("");
+    }
+    BUCKET_SIZE *= 2;
+}
 
+//recalculates the vector index of each word using the new bucket size
+//called after resizeVector when load factor exceeds the maximum
+void HashTable::rehashWords(){
+    for(int i = 0; i < BUCKET_SIZE; i++){
+        vec[i] = "";
+    }
+
+    for(auto it = table.begin(); it != table.end(); it++){
+        int index = hashFunc(it->first);
+
+        if(vec[index] == ""){
+            vec[index] = it->second;
+        }
+
+        else{
+            index = collisionReduction(index);
+            vec[index] = it->second;
+        }
+    }
+}
+
+//inserts a given word into the hash table map and vector
+//assigns word to the map key and vector index using computeKey and hashFunc
+//calls collisionReduction if necessary to resolve collisions
+//calls resizeVector and rehashWords as necessary
+void HashTable::insertWord(string word){
     int key = computeKey(word);
     table[key] = word;
 
@@ -97,9 +144,10 @@ void HashTable::insertWord(string word){
 
 }
 
-void HashTable::insertAll(){
+//inserts every word from a given text file into the map
+void HashTable::insertAll(string filename){
     ifstream file;
-    file.open("../englishWords.txt");
+    file.open(filename);
 
     string word;
     while(getline(file, word)){
@@ -109,32 +157,7 @@ void HashTable::insertAll(){
     file.close();
 }
 
-void HashTable::resizeVector(){
-    for(int i = BUCKET_SIZE; i < BUCKET_SIZE * 2; i++){
-        vec.push_back("");
-    }
-    BUCKET_SIZE *= 2;
-}
-
-void HashTable::rehashWords(){
-    for(int i = 0; i < BUCKET_SIZE; i++){
-        vec[i] = "";
-    }
-
-    for(auto it = table.begin(); it != table.end(); it++){
-        int index = hashFunc(it->first);
-
-        if(vec[index] == ""){
-            vec[index] = it->second;
-        }
-
-        else{
-            index = collisionReduction(index);
-            vec[index] = it->second;
-        }
-    }
-}
-
+//searches for the given input string in the map using unordered_map::find(key)
 bool HashTable::searchWord(string input){
     int key = computeKey(input);
     if(table.find(key) != table.end()){
